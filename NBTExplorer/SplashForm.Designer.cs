ba08afd66783c37b61a456c46b5c01e76b975bd4
kokkiemouse <39451248+kokkiemouse@users.noreply.github.com>
2018-10-08 08:52:52 +0900

    Add files via upload

---
+++ NBTExplorer/SplashForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace NBTExplorer
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox1.ErrorImage = global::NBTExplorer.Properties.Resources.ArrowDown;
+            this.pictureBox1.Image = global::NBTExplorer.Properties.Resources.Splash;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(730, 394);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SplashForm";
+            this.Text = "SplashForm";
+            this.Load += new System.EventHandler(this.SplashForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ NBTExplorer/SplashForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NBTExplorer
+{
+    public partial class SplashForm : Form
+    {
+        //Splashフォーム
+        private static SplashForm _form = null;
+        //メインフォーム
+        private static Form _mainForm = null;
+        //Splashを表示するスレッド
+        private static System.Threading.Thread _thread = null;
+        //lock用のオブジェクト
+        private static readonly object syncObject = new object();
+        //Splashが表示されるまで待機するための待機ハンドル
+        private static System.Threading.ManualResetEvent splashShownEvent = null;
+
+        /// <summary>
+        /// Splashフォーム
+        /// </summary>
+        public static SplashForm Form
+        {
+            get { return _form; }
+        }
+
+        /// <summary>
+        /// Splashフォームを表示する
+        /// </summary>
+        /// <param name="mainForm">メインフォーム</param>
+        public static void ShowSplash(Form mainForm)
+        {
+            lock (syncObject)
+            {
+                if (_form != null || _thread != null)
+                {
+                    return;
+                }
+
+                _mainForm = mainForm;
+                //メインフォームのActivatedイベントでSplashフォームを消す
+                if (_mainForm != null)
+                {
+                    _mainForm.Activated += new EventHandler(_mainForm_Activated);
+                }
+
+                //待機ハンドルの作成
+                splashShownEvent = new System.Threading.ManualResetEvent(false);
+
+                //スレッドの作成
+                _thread = new System.Threading.Thread(
+                    new System.Threading.ThreadStart(StartThread));
+                _thread.Name = "SplashForm";
+                _thread.IsBackground = true;
+                _thread.SetApartmentState(System.Threading.ApartmentState.STA);
+                //.NET Framework 1.1以前では、以下のようにする
+                //_thread.ApartmentState = System.Threading.ApartmentState.STA;
+                //スレッドの開始
+                _thread.Start();
+            }
+        }
+
+        /// <summary>
+        /// Splashフォームを表示する
+        /// </summary>
+        public static void ShowSplash()
+        {
+            ShowSplash(null);
+        }
+
+        /// <summary>
+        /// Splashフォームを消す
+        /// </summary>
+        public static void CloseSplash()
+        {
+            lock (syncObject)
+            {
+                if (_thread == null)
+                {
+                    return;
+                }
+
+                if (_mainForm != null)
+                {
+                    _mainForm.Activated -= new EventHandler(_mainForm_Activated);
+                }
+
+                //Splashが表示されるまで待機する
+                if (splashShownEvent != null)
+                {
+                    splashShownEvent.WaitOne();
+                    splashShownEvent.Close();
+                    splashShownEvent = null;
+                }
+
+                //Splashフォームを閉じる
+                //Invokeが必要か調べる
+                if (_form != null)
+                {
+                    if (_form.InvokeRequired)
+                    {
+                        _form.Invoke(new MethodInvoker(CloseSplashForm));
+                    }
+                    else
+                    {
+                        CloseSplashForm();
+                    }
+                }
+
+                //メインフォームをアクティブにする
+                if (_mainForm != null)
+                {
+                    if (_mainForm.InvokeRequired)
+                    {
+                        _mainForm.Invoke(new MethodInvoker(ActivateMainForm));
+                    }
+                    else
+                    {
+                        ActivateMainForm();
+                    }
+                }
+
+                _form = null;
+                _thread = null;
+                _mainForm = null;
+            }
+        }
+
+        //スレッドで開始するメソッド
+        private static void StartThread()
+        {
+            //Splashフォームを作成
+            _form = new SplashForm();
+            //Splashフォームをクリックして閉じられるようにする
+            _form.Click += new EventHandler(_form_Click);
+            //Splashが表示されるまでCloseSplashメソッドをブロックする
+            _form.Activated += new EventHandler(_form_Activated);
+            //Splashフォームを表示する
+            Application.Run(_form);
+        }
+
+        //SplashのCloseメソッドを呼び出す
+        private static void CloseSplashForm()
+        {
+            if (!_form.IsDisposed)
+            {
+                _form.Close();
+            }
+        }
+
+        //メインフォームのActivateメソッドを呼び出す
+        private static void ActivateMainForm()
+        {
+            if (!_mainForm.IsDisposed)
+            {
+                _mainForm.Activate();
+            }
+        }
+
+        //Splashフォームがクリックされた時
+        private static void _form_Click(object sender, EventArgs e)
+        {
+            //Splashフォームを閉じる
+            CloseSplash();
+        }
+
+        //メインフォームがアクティブになった時
+        private static void _mainForm_Activated(object sender, EventArgs e)
+        {
+            //Splashフォームを閉じる
+            CloseSplash();
+        }
+
+        //Splashフォームが表示された時
+        private static void _form_Activated(object sender, EventArgs e)
+        {
+            _form.Activated -= new EventHandler(_form_Activated);
+            //CloseSplashメソッドの待機を解除
+            if (splashShownEvent != null)
+            {
+                splashShownEvent.Set();
+            }
+        }
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
